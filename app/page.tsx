d9f5a58f03efbe8baa573912786b4bd7dd43acb0
Shelwin <shelwinsunga@gmail.com>
2024-10-12 14:24:58 -0700

    add content collections

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import { allPosts } from 'content-collections';
+
 export const maxDuration = 30;
 
 export default function Home() {
-
   return (
-    <div>
-      Hello
-    </div>
+    <ul>
+      {allPosts.map((post) => (
+        <li key={post._meta.path}>
+          <a href={`/posts/${post._meta.path}`}>
+            <h3>{post.title}</h3>
+            <p>{post.summary}</p>
+          </a>
+        </li>
+      ))}
+    </ul>
   );
 }
