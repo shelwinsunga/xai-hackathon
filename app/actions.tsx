dd82b2c9dd77cd4464720a8cc91fceadb1a90983
Shelwin <shelwinsunga@gmail.com>
2024-10-12 09:40:46 -0700

    pre-hack setup

---
+++ app/actions.tsx
@@ -0,0 +1,48 @@
+'use server';
+
+import { streamUI } from 'ai/rsc';
+import { anthropic } from '@ai-sdk/anthropic';
+import { z } from 'zod';
+
+const LoadingComponent = () => (
+  <div className="animate-pulse p-4">getting weather...</div>
+);
+
+const getWeather = async (location: string) => {
+  await new Promise(resolve => setTimeout(resolve, 2000));
+  return '82°F️ ☀️';
+};
+
+interface WeatherProps {
+  location: string;
+  weather: string;
+}
+
+const WeatherComponent = (props: WeatherProps) => (
+  <div className="border border-neutral-200 p-4 rounded-lg max-w-fit">
+    The weather in {props.location} is {props.weather}
+  </div>
+);
+
+export async function streamComponent() {
+  const result = await streamUI({
+    model: anthropic('claude-3-5-sonnet-20240620'),
+    prompt: 'Get the weather for San Francisco',
+    text: ({ content }) => <div>{content}</div>,
+    tools: {
+      getWeather: {
+        description: 'Get the weather for a location',
+        parameters: z.object({
+          location: z.string(),
+        }),
+        generate: async function* ({ location }) {
+          yield <LoadingComponent />;
+          const weather = await getWeather(location);
+          return <WeatherComponent weather={weather} location={location} />;
+        },
+      },
+    },
+  });
+
+  return result.value;
+}
