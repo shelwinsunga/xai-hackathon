114932762a422d5c70e52ad583b351bff663a9bb
Shelwin <shelwinsunga@gmail.com>
2024-10-12 14:32:29 -0700

    starter

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,8 @@ import { Button } from "@/components/ui/button";
 const components = { Button };
 
 export default function App() {
-  console.log(allPosts);
   return (
     <main>
-      <h1>Posts</h1>
       <ul>
         {allPosts.map((post) => (
           <li key={post._meta.path}>
