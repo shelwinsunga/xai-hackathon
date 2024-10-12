78d5c1c21c5f7cb220b02fade2989257ef7ecea1
Shelwin <shelwinsunga@gmail.com>
2024-10-12 14:31:04 -0700

    infra for gen-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { allPosts } from "content-collections";
 import { MDXContent } from "@content-collections/mdx/react";
- 
+import { Button } from "@/components/ui/button";
+const components = { Button };
+
 export default function App() {
   console.log(allPosts);
   return (
@@ -11,7 +13,7 @@ export default function App() {
         {allPosts.map((post) => (
           <li key={post._meta.path}>
             <h2>{post.title}</h2>
-            <MDXContent code={post.mdx} />
+            <MDXContent code={post.mdx} components={components} />
           </li>
         ))}
       </ul>
