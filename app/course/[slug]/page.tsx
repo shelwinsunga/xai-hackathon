0add3cd4a72539371b2448822533842e922e964f
Shelwin <shelwinsunga@gmail.com>
2024-10-12 18:22:38 -0700

    back button

--- app/course/[slug]/page.tsx
+++ app/course/[slug]/page.tsx
@@ -4,12 +4,14 @@ import { MDXContent } from "@content-collections/mdx/react";
 import { Button } from "@/components/ui/button";
 import FRQ from "@/components/frq";
 import { formatDate } from "@/app/utils";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
+import Link from "next/link";
 
-const components = { FRQ };
+const components = { FRQ, Link };
 
 export default function Course() {
   const params = useParams();
+  const router = useRouter();
   const slug = params.slug as string;
   
   const post = allPosts.find(post => post._meta.path === `${slug}`);
@@ -23,7 +25,10 @@ export default function Course() {
     <div className="flex items-center justify-center min-h-[60vh]">
       <div className="w-full px-4">
         <article className="mb-8">
-          <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
+          <span className="flex justify-between items-center">
+            <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
+            <Button variant="ghost" className="text-muted-foreground underline" onClick={() => router.push('/')}>Back</Button>
+          </span>
           <p className="text-lg text-muted-foreground mb-4">By {post.author} • {formatDate(post.date || '')}</p>
           <div className="prose">
             <MDXContent code={post.mdx} components={components} />
